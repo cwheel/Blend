d63685f52f8c0ff6158f93e4d58593ba690dd05e
Cameron Wheeler <cwheel@users.noreply.github.com>
2022-12-29 10:35:47 -0500

    Initial Commit

---
+++ Blend/BlendApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlendApp.swift
+//  Blend
+//
+//  Created by Cameron Wheeler on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct BlendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
